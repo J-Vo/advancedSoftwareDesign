529de0455e804d2116d669e119fc2a42eea3a21c
J-Vo <jordanvolkman@gmail.com>
2018-09-25 16:24:11 -0500

    added comments.

--- advancedSoftwareDesign/src/assignment2/Complex.java
+++ advancedSoftwareDesign/src/assignment2/Complex.java
@@ -1,12 +1,22 @@
+//Name: Jordan Volkman
+//Class: Advanced Software Design
+//Date: 9/25/2018
+//Requirements:
+/*Write a program to test your class that does the following:
+  -Prompt the user to enter the real and imaginary part of a complex number. 
+  -Prompt the user to enter the real and imaginary part of a second complex number. 
+  -Print the result of adding the two numbersusing the print method.
+  -Print the resultof subtracting the two numbers using the print method.*/
+
 package assignment2;
 
 import java.util.Scanner;
 
 public class Complex {
-
+		// declare class variables.
 		private double realPart;
 		private double imaginaryPart;
-		
+		// methods for setters and getters.
 		public double getRealPart() {
 			return realPart;
 		}
@@ -19,67 +29,70 @@ public class Complex {
 		public void setImaginaryPart(double imaginaryPart) {
 			this.imaginaryPart = imaginaryPart;
 		}
-
+		// no-argument constructor
 		Complex(){
-			this.realPart = 0;
-			this.imaginaryPart = 0;
+			this(0,0);
 		}
+		// two-argument constructor.
 		Complex(double real, double imaginary){
 			this.realPart = real;
 			this.imaginaryPart = imaginary;
 		}
-		
-		public Complex addNumbers(Complex number)
-		{
+		// add number method
+		public Complex addNumbers(Complex number){
+			// instantiate holder object.
 			Complex resultNum = new Complex(); 
-
+			//perform calculation
 			resultNum.setRealPart(this.getRealPart() + number.getRealPart());
 			resultNum.setImaginaryPart(this.getImaginaryPart() + number.getImaginaryPart());
-			
+			//return results
 			return resultNum;
 		}
 		public String toString() {
+			//Set toString format for printing.
 			return String.format("(%.1f, %.1f)", this.getRealPart(), this.getImaginaryPart());
 		}
 		
 		public Complex subtractNumbers(Complex number){
-				
+			//instantiate local object to hold results.
 			Complex resultNum = new Complex(); 
-
+			//perform calculation
 			resultNum.setRealPart(this.getRealPart() - number.getRealPart());
 			resultNum.setImaginaryPart(this.getImaginaryPart() - number.getImaginaryPart());
-				
+			//return results
 			return resultNum;
 		}
+		//Main method to drive the program.
 		public static void main(String[] args) {
-			
+			// declare variables.
 			double inputx;
 			double inputy;
-			
+			// instatiate input object.
 			Scanner input = new Scanner(System.in);
 			
 			System.out.println("Enter the real part and imaginary part of a complex number, starting with the real number:");
-			
+			//Grab user input.
 			inputx = input.nextDouble();
 			
 			inputy = input.nextDouble();
-			
+			//instantiate first Complex class.
 			Complex c1 = new Complex(inputx, inputy);
 			
 			input.nextLine();
 			
 			System.out.println("Enter the real part and imaginary part of a second complex number, starting with the real number:");
-			
+			//gather input for c2 object.
 			inputx = input.nextDouble();
 			
 			inputy = input.nextDouble();
-			
+			// Instatiate second object
 			Complex c2 = new Complex(inputx, inputy);
-			
+			//Instantiate third object to input the results into.
 			Complex c3 = new Complex();
-			
+			// add numbers input into c3.
 			c3 = c1.addNumbers(c2);
 			System.out.printf("Addition result: %s%n" , c3.toString());
+			// subtract numbers and overwrite results from addition into c3.
 			c3 = c1.subtractNumbers(c2);
 			System.out.printf("Subtraction result: %s%n" , c3.toString());
 
--- advancedSoftwareDesign/src/assignment2/Rational.java
+++ advancedSoftwareDesign/src/assignment2/Rational.java
@@ -1,14 +1,26 @@
-
+//Name: Jordan Volkman
+//Class: Advanced Software Design
+//Date: 9/25/2018
+//Requirements:
+/* Write a program to test your classthat does the following:
+ * Prompt the user to enter the numerator and denominator of a rational number.
+ * Prompt the user to enter the numerator and denominator of a second rational number.
+ * Prompt the user to enter the number of digits of precision to display to the right of the decimal point.
+ * Print the result of adding the two rational numbersin the form a/band floating-point format.
+ * Print the result of subtracting the two rational numbers in the form a/band floating-point format. 
+ * Print the result of multiplying the two rational numbers in the form a/band floating-point format. 
+ * Print the result of dividing the two rational numbers in the form a/band floating-point format
+ */
 package assignment2;
 
 import java.util.Scanner;
 
 public class Rational {
-	//DECLARE 2 PRIVATE VARIABLES
+	// declare 2 private class variables
 	private int numerator;
 	private int denominator;
 	
-	// CREATE SETTERS AND GETTERS
+	// create setters and getters
 	public int getNumerator() {
 		return numerator;
 	}
@@ -21,20 +33,20 @@ public class Rational {
 	public void setDenominator(int denominator) {
 		this.denominator = denominator;
 	}
-	//NO-ARGUMENT CONSTRUCTOR
+	// no-argument constructor
 	Rational(){
-		this.numerator = 0;
-		this.denominator = 0;
+		this(0,0);
 	}
-	//2 ARGUMENT CONSTRUCTOR
+	// two-argument constructor
 	Rational(int num, int den){
 		this.numerator = num;
 		this.denominator = den;
 	}
-	//ADDING TWO RATIONAL NUMBERS
+	// adding two rational numbers
 	public static Rational addRational(Rational number1, Rational number2){
 		//initialize holder object.
 		Rational rat1 = new Rational();
+		// declare holder variables.
 		int num1den;
 		int num2den;
 		int num1num;
@@ -44,26 +56,27 @@ public class Rational {
 		
 			num1den = number1.getDenominator() * number2.getDenominator();
 			num2den = number1.getDenominator() * number2.getDenominator();
-			
+
 			num1num = number1.getNumerator() * number2.getDenominator();
 			num2num = number2.getNumerator() * number1.getDenominator();
 			
 			rat1.setNumerator(num1num + num2num);
 			rat1.setDenominator(num1den);
-		}
+		} // if the denominators are not the same, make them the same and add.
 		else{
 			rat1.setNumerator(number1.getNumerator() + number2.getNumerator());
 			rat1.setDenominator(number1.getDenominator());
-		}
-		
+		}// if they are the same, do the calculation.
+		// simplify the fraction
 		simplify(rat1);
-		
+		// return local object.
 		return rat1;
 	}
-	//SUBTRACT RATIONAL NUMBERS
+	// subtract rational numbers
 	public static Rational subtractRational(Rational number1, Rational number2) {
-		
+		// initialize holder object.
 		Rational rat1 = new Rational();
+		// declare holder variables.
 		int num1den;
 		int num2den;
 		int num1num;
@@ -76,49 +89,54 @@ public class Rational {
 			
 			num1num = number1.getNumerator() * number2.getDenominator();
 			num2num = number2.getNumerator() * number1.getDenominator();
-			
+			// subtract after making fractions equivalent
 			rat1.setNumerator(num1num - num2num);
 			rat1.setDenominator(num1den);
 		}
 		else{
+			//subtract without having to do conversion
 			rat1.setNumerator(number1.getNumerator() - number2.getNumerator());
 			rat1.setDenominator(number1.getDenominator());
 		}
-		
+		// simplify result if possible
 		simplify(rat1);
 		
 		return rat1;
 	}
+	// Divide rational numbers
 	public static Rational divideRational(Rational number1, Rational number2){
+		// initialize holder object.
 		Rational rat1 = new Rational();
-		
+		// declare holder variables.
 		int num;
 		int den;
 		
 		num = number2.getDenominator();
 		den = number2.getNumerator();
-		
+		// make the fraction a reciprocal
 		rat1.setNumerator(num);
 		rat1.setDenominator(den);
-		
+		// multiply result of reciprocal
 		return multiplyRational(number1, rat1);
 	}
+	//multiply rational numbers
 	public static Rational multiplyRational(Rational number1, Rational number2){
+		//initialize holder object.
 		Rational rat1 = new Rational();
-		
+		// perform mutliplication
 		rat1.setNumerator(number1.getNumerator() * number2.getNumerator());
 		rat1.setDenominator(number1.getDenominator() * number2.getDenominator());
-		
+		// simplify
 		simplify(rat1);
 		
 		return rat1;
 	}
 	public String toString(int precision) {
-		
+		// declare holder variables.
 		double num = this.getNumerator();
 		double den = this.getDenominator();
 		double floatPoint = num / den;
-		
+		// format toString output.
 		return String.format("%d/%d, %."+ precision + "f", this.numerator, this.denominator, floatPoint);
 	}
 	public static void simplify(Rational number1){
@@ -127,55 +145,60 @@ public class Rational {
 			
 			number1.setDenominator(1);
 			number1.setNumerator(1);
-		}
+		}// if numerator and denominator are the same, return 1
 		else{
-			
+			// find the greatest common multiple through this static method. 
 		long gcm = greatestCommonMultiple(number1.getNumerator(), number1.getDenominator());
-		
+		// set numerator and denominator by first dividing out the gcm.
 		number1.setNumerator((int) (number1.getNumerator() / gcm));
 		number1.setDenominator((int) (number1.getDenominator() / gcm));
 		}
 			
 	}
 	public static long greatestCommonMultiple(long numerator, long denominator) {
+		// return gcm between the two inputs, keep looping until found.
 	    return denominator == 0 ? numerator : greatestCommonMultiple(denominator, numerator % denominator);
 	}
 	
 	public static void main(String[] args) {
-		
+		// main method to drive the program.
+		// initialize holder variables for user input.
 		int a;
 		int b;
 		int precision = 2;
-		
+		// instantiate scanner object
 		Scanner input = new Scanner(System.in);
 		
+		// grab input
 		System.out.println("Enter your first rational number's numerator and denominator separated by a space.");
 		a = input.nextInt();
 		b = input.nextInt();
 		
 		System.out.printf("%d%d%n", a, b);
 		Rational num1 = new Rational(a, b);
-		
+		// create new instance of object from user input
 		input.nextLine();
 		
 		System.out.println("Enter your second rational number's numerator and denominator separated by a space.");
 		a = input.nextInt();
 		b = input.nextInt();
+		// create new instance of object from user input
 		Rational num2 = new Rational(a, b);
+		// gather user input for decimal precision
 		System.out.println("Enter the number of decimal places you would like to see in your answers.");
 		precision = input.nextInt();
-		
+		// create new instance of object for result holding purposes
 		Rational num3 = new Rational();
-		
+		// add numbers
 		num3 = addRational(num1, num2);
 		System.out.println(num3.toString(precision));
-		
+		// subtract numbers
 		num3 = subtractRational(num1, num2);
 		System.out.println(num3.toString(precision));
-		
+		// multiply numbers
 		num3 = multiplyRational(num1, num2);
 		System.out.println(num3.toString(precision));
-		
+		// divide numbers
 		num3 = divideRational(num1, num2);	
 		System.out.println(num3.toString(precision));
 		
